67027beb8fcd837b4e6118c29d2f1b75269f4789
Kristen Shea <kstar61@live.com>
2018-09-19 18:07:52 -0500

    *

--- CG 11-1 (Student Names)/Student.cs
+++ CG 11-1 (Student Names)/Student.cs
@@ -6,7 +6,7 @@ namespace CG_11_1__Student_Names_
 {
     public class Student
     {
-        //public int ExamScore { get; set; }
+        public int ExamScore { get; set; }
 
         //property will be a list of int
 
