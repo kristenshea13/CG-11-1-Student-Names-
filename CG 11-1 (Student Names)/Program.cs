6822f2afa99b05911b1d497b1933223ee398ebba
Kristen Shea <kstar61@live.com>
2018-09-20 19:13:36 -0500

    *

--- CG 11-1 (Student Names)/Program.cs
+++ CG 11-1 (Student Names)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CG_11_1__Student_Names_
 {
@@ -6,12 +7,34 @@ namespace CG_11_1__Student_Names_
     {
         static void Main(string[] args)
         {
+
+            //Using the text file studentdata.txt write a program that prints out the names of
+            //students that have more than six quiz scores.Be sure to use a Student class.
+
+
+            Student student = new Student();
             
 
 
 
+            
+
+            string[] students = File.ReadAllLines(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
+
+            //foreach (string item in students)
+            //{
+                
 
 
+
+
+            //}
+
+
+            //StreamReader studentinfo = new StreamReader(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
+
+
+            Console.ReadLine();
         }
     }
 }
--- CG 11-1 (Student Names)/Student.cs
+++ CG 11-1 (Student Names)/Student.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CG_11_1__Student_Names_
 {
     public class Student
     {
-        public int ExamScore { get; set; }
 
-        //property will be a list of int
+        public string Name { get; set; }
+        
+        public int Score { get; set; }
+
+        //public string StudentData { get; set; }
+
+        //    StreamReader studentinfo = new StreamReader(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
+
+        //    public string ReadData()
+        //    {
+        //        StudentData = studentinfo.ReadLine();
+        //        return StudentData;
+        //    }
 
         
 
 
-
-
-
-    }
+        }
 }
