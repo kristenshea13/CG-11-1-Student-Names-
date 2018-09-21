14f91a5b941e3c2c824964c541e2c1f00b6bcb10
Kristen Shea <kstar61@live.com>
2018-09-20 23:02:17 -0500

    updated comments

--- CG 11-1 (Student Names)/Program.cs
+++ CG 11-1 (Student Names)/Program.cs
@@ -9,25 +9,29 @@ namespace CG_11_1__Student_Names_
         static void Main(string[] args)
         {
 
-            //Using the text file studentdata.txt write a program that prints out the names of
-            //students that have more than six quiz scores.Be sure to use a Student class.
+           
 
-
+            //read the data from the text file
             string[] students = File.ReadAllLines(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
 
+            //initiate new list for all the studentData for each student created
             List<Student> studentData = new List<Student>();
 
+            //foreach loop to add info to each student in list of studentData
             foreach (string item in students)
             {
                 studentData.Add(new Student(item));
-
             }
 
             Console.WriteLine("These students have more than 6 Exam scores: ");
+
             foreach (Student student in studentData)
             {
+                //foreach loop to loop through students in studentData list and print each name
+                //that has more than 6 exam scores
                 if (student.Scores.Count > 6)
                 {
+                    //capitalize the first letter in each name that prints
                     string studentCap = char.ToUpper(student.Name[0]) + student.Name.Substring(1);
                     Console.WriteLine(studentCap);
                 }
@@ -35,8 +39,7 @@ namespace CG_11_1__Student_Names_
             }
 
 
-            //StreamReader studentinfo = new StreamReader(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
-
+            
 
             Console.ReadLine();
         }
--- CG 11-1 (Student Names)/Student.cs
+++ CG 11-1 (Student Names)/Student.cs
@@ -7,15 +7,21 @@ namespace CG_11_1__Student_Names_
 {
     public class Student
     {
-
+        //properties for student class: string Name, and int List of scores
         public string Name { get; set; }
-        
+
         public List<int> Scores { get; set; }
 
         public Student(string studentData)
         {
+            //initiate string for splitting data from text file
             string[] studentdataSplit = studentData.Split("; ");
+
+            //initiate the list of scores
             Scores = new List<int>();
+
+            //designate first item on each line as name, 
+            //and other items on each line as scores
             for (int i = 0; i < studentdataSplit.Length; i++)
             {
                 if (i == 0)
@@ -27,25 +33,11 @@ namespace CG_11_1__Student_Names_
                     int score = int.Parse(studentdataSplit[i]);
                     Scores.Add(score);
                 }
-                
 
             }
 
         }
 
 
-        //public string StudentData { get; set; }
-
-        //    StreamReader studentinfo = new StreamReader(@"C:\Users\kstar\source\repos\CG 11-1 (Student Names)\CG 11-1 (Student Names)\StudentData.txt");
-
-        //    public string ReadData()
-        //    {
-        //        StudentData = studentinfo.ReadLine();
-        //        return StudentData;
-        //    }
-
-        
-
-
-        }
+    }   
 }
